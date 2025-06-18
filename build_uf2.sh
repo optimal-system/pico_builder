09c60a985e3290341cbc7b444b746c05a5e11709
lighta <lightaisme@gmail.com>
2025-06-18 14:37:40 -0400

    Update to use ghcr.io/lighta/micropython-picow-builder:latest instead

--- build_uf2.sh
+++ build_uf2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 #Simple helper permettant de convertire le module/main.py en firmware.uf2
 set -e
+docker pull ghcr.io/lighta/micropython-picow-builder:latest
 echo "=== BUILDING MICROPYTHON FIRMWARE FOR PICO (W and 2W) ==="
 docker run --rm \
     -v $(pwd)/module:/module \
     -v $(pwd)/firmware:/firmware \
-    --entrypoint /opt/build.sh micropython-pico-builder
+    --entrypoint /opt/build.sh ghcr.io/lighta/micropython-picow-builder:latest
 echo "=== Result should be in /firmware folder ==="
